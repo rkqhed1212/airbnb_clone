ede24d41dd171272033dd2b102bed807426a4aa8
shane <shane@gmail.com>
2021-06-02 09:18:46 +0900

    push

--- users/forms.py
+++ users/forms.py
@@ -1,11 +1,13 @@
 from django import forms 
-from django.contrib.auth.forms import UserCreationForm
+
 from . import models
 
 class LoginForm(forms.Form):
 
-    email = forms.EmailField()
-    password = forms.CharField(widget = forms.PasswordInput)
+    email = forms.EmailField(widget=forms.EmailInput(attrs={"placeholder": "Email"}))
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={"placeholder": "Password"})
+    )
 
             
     def clean(self):
@@ -22,6 +24,35 @@ class LoginForm(forms.Form):
                 self.add_error("email", forms.ValidationErorr("User does not exist"))
 
 
-class SignUpForm(UserCreationForm):
+class SignUpForm(forms.ModelForm):
 
-    username = forms.EmailField(label="Email")
+    class Meta:
+        model = models.User
+        fields = ("first_name", "last_name", "email")
+
+        widgets = {
+            "first_name": forms.TextInput(attrs={"placeholder": "First Name"}),
+            "last_name": forms.TextInput(attrs={"placeholder": "Last Name"}),
+            "email": forms.EmailInput(attrs={"placeholder": "Email Name"}),
+        }
+
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={"placeholder": "Password"})
+    )
+    password1 = forms.CharField(
+        widget=forms.PasswordInput(attrs={"placeholder": "Confirm Password"})
+    )
+
+    def clean_email(self):
+        email = self.cleaned_data.get("email")
+        try:
+            models.User.objects.get(email=email)
+            raise forms.ValidationError(
+                "That email is already taken", code="existing_user"
+            )
+        except models.User.DoesNotExist:
+            return email
+
+    def clean_password1(self):
+        password = self.cleaned_data.get("password")
+        password1 = self.cleaned_data.get("password1")
--- users/views.py
+++ users/views.py
@@ -37,12 +37,7 @@ class SignUpView(FormView):
     template_name = "users/signup.html"
     form_class = forms.SignUpForm
     success_url = reverse_lazy("core:home")
-    initial = {
-        'first_name': "Shane",
-        'last_name': 'Kim',
-        'email': 'rkqehd12@gmail.com'
-    }
-    
+
     def form_valid(self, form):
         form.save()
         email = form.cleaned_data.get("email")
